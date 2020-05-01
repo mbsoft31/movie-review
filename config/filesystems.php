54f7527d0a0b1f3c6baf6d181041c42e427644ce
Mouadh Bekhouche <bekhouche.mouadh@gmail.com>
2020-05-01 09:21:12 +0100

    change cpanel.yaml

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('uploads'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
@@ -80,7 +80,6 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        '/public_html/storage' => storage_path('app/public'),
     ],
 
 ];
